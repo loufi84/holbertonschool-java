7cba290c16b4ee871ac0158396511f91ea40b7f7
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-13 11:44:05 +0200

    Task 4

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main(String[] args) {
         String text1 = "Holberton";
         String text2 = "School";
-        String fullText = text1 + text2;
-        System.out.printf("Welcome to " + fullText + " !%n");
+        String fullText = text1 + " " + text2;
+        System.out.println("Welcome to " + fullText + " !");
     }
 }
