3fea3e3135f3883b5f354e993a4e5bb49e64a27d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-13 13:21:04 +0200

    Task 0

--- 0x02/division/Number.java
+++ 0x02/division/Number.java
@@ -1,12 +1,14 @@
-class TheNumber {
-    public static void todivide(int a, int b) {
-        int res = 0;
-        try {
-            res = a / b;
-        } catch (Exception e) {
-            System.out.println("It is not possible to divide by zero.");
-        } finally {
-            System.out.printf("%d / %d = %d", a, b, res);
+public class Number {
+    public class TheNumber {
+        public static void todivide(int a, int b) {
+            int res = 0;
+            try {
+                res = a / b;
+            } catch (Exception e) {
+                System.out.println("It is not possible to divide by zero.");
+            } finally {
+                System.out.printf("%d / %d = %d", a, b, res);
+            }
         }
     }
 }
