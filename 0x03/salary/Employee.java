f0668bfac3c52ba553b91a848737393c0719fdc3
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-13 17:24:18 +0200

    Task 0

--- 0x03/salary/Employee.java
+++ 0x03/salary/Employee.java
@@ -12,8 +12,9 @@ public class Employee {
     public double calculateBonus(Department department) {
         if (department.reachedTarget()) {
             return fixedSalary * 10 / 100;
+        } else {
+            return 0.0;
         }
-        return 0.0;
     }
 
     public double calculateTotalSalary(Department department) {
