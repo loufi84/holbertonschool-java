5df76306acff3f0951b3e242ae236e118bbd3763
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-13 17:16:30 +0200

    Task 0

--- 0x03/salary/Manager.java
+++ 0x03/salary/Manager.java
@@ -8,7 +8,7 @@ public class Manager extends Employee {
     public double calculateBonus(Department department) {
         if (department.reachedTarget()) {
             double baseBonus = getFixedSalary() * 20 / 100;
-            double additionalBonus = (department.getAchievedTargetValue() - department.getTargetValue()) * 0.01;
+            double additionalBonus = (department.getAchievedTargetValue() - department.getTargetValue()) * 1 / 100;
             return baseBonus + additionalBonus;
         }
         return 0.0;
