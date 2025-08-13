677926c80aff5ee7b98d17048cbeaa5659863f72
Quentin <48597772+loufi84@users.noreply.github.com>
2025-08-13 13:15:21 +0200

    Task 0

--- 0x02/division/Number.java
+++ 0x02/division/Number.java
@@ -4,11 +4,11 @@ public class Program {
 
         a = 4;
         b = 3;
-        Number.todivide(a, b);
+        TheNumber.todivide(a, b);
         System.out.println();
 
         a = 9;
         b = 0;
-        Number.todivide(a, b);
+        TheNumber.todivide(a, b);
     }
 }
